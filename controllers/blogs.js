7309de403ac7c800c3b63007d08daec108cbe820
Anton Fredriksson <a.o.fredriksson@gmail.com>
2023-01-30 14:24:55 +0200

    Join and populate blogs & users

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,20 +1,25 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogsRouter.get('/', (req, res) => {
-    Blog.find({})
-        .then(blogs => {
-            res.json(blogs)
-        })
+blogsRouter.get('/', async (req, res) => {
+    const blogs = await Blog
+        .find({}).populate('user', { username: 1, name: 1, id: 1})
+    
+    res.json(blogs)
 })
 
-blogsRouter.post('/', (req, res) => {
-    const blog = new Blog(req.body)
+blogsRouter.post('/', async (req, res) => {
+    const body = req.body
+    const user = await User.findById(body.userId)
+    const blog = new Blog(body)
+    blog.user = user._id
 
-    blog.save()
-        .then(result => {
-            res.status(201).json(result)
-        })
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    res.json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (req, res) => {
