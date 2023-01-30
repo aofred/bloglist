cebae39c71042379c1e6e6d76f7329dbc99da119
Anton Fredriksson <a.o.fredriksson@gmail.com>
2023-01-30 17:21:05 +0200

    Login auth JWT

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {  
+    const authorization = request.get('authorization')  
+    if (authorization && authorization.startsWith('Bearer ')) {    
+        return authorization.replace('Bearer ', '')  
+    }  
+    return null
+}
 
 blogsRouter.get('/', async (req, res) => {
     const blogs = await Blog
@@ -11,10 +20,15 @@ blogsRouter.get('/', async (req, res) => {
 
 blogsRouter.post('/', async (req, res) => {
     const body = req.body
-    const user = await User.findById(body.userId)
+    
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {    
+        return response.status(401).json({ error: 'token invalid' })  
+    }
+    const user = await User.findById(decodedToken.id)
+    
     const blog = new Blog(body)
     blog.user = user._id
-
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
